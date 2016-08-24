b03d7ffc155b7d085841be79eeffac3eb21ff6bb
wkdchdaud <wkdchdaud2@naver.com>
2016-08-24 13:18:41 +0900

    add comments about add func to calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,7 +1,7 @@
 
 
 public class Calc {
-        public int add(int x, int y) {
+        public int add(int x, int y) {//add method
 	                return x+y;
 			                }
 					        }
