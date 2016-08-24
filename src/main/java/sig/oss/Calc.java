8a26e175bca89a293961a810e0e8ef2ab7068f63
wkdchdaud <wkdchdaud2@naver.com>
2016-08-24 13:30:33 +0900

    add OR subtraction meyhod and test code to Calc and CalTest

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -4,5 +4,12 @@ public class Calc {
         public int add(int x, int y) {//add method
 	                return x+y;
 			                }
-					        }
-
+	
+	public int sub(int x, int y) {//subscription method
+		                        return x-y;
+					                                   
+		
+		
+		
+		}
+		
